753115fd07642d43e88159f0d8c3bae8309e5a7b
james <jayrh3246@gmail.com>
2021-03-08 09:47:15 -0500

    commit tree 5ba1fb6f8235d8bd85e861374194b568282114fc

--- solution.rb
+++ solution.rb
@@ -1,7 +1,38 @@
 class Deck
 
+
+    attr_accessor :cards
+    @@all = []
+
+      def initialize
+        @cards = []
+        for suit in ["Hearts", "Clubs", "Diamonds", "Spades"] do
+        for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] do
+            @cards << Card.new(suit, rank)
+            end
+        end
+        #binding.pry
+    end
+
+    def choose_card
+        
+    end
+
+    def self.all
+        @@all
+    end
 end
 
 class Card
 
+    attr_reader :rank, :suit
+
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit
+    end
+
+    def self.all
+        @@all
+    end
 end
